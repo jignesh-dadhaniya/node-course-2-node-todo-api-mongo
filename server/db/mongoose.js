4b364543a6abf15c64b49ede9e62cb3e10d769cd
jignesh-russmediatech <jignesh.dadhaniya@russmediatech.in>
2018-11-23 18:25:54 +0530

    remove method added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp2', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = {mongoose};
+
+//process.env.NODE_ENV === 'development'
