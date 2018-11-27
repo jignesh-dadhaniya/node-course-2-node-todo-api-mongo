c33076e7a54a0c7ae2bc0ca3683d7e60d0ecc67a
jignesh-russmediatech <jignesh.dadhaniya@russmediatech.in>
2018-11-27 18:30:46 +0530

    login method added

--- server/server.js
+++ server/server.js
@@ -120,9 +120,25 @@ app.post('/users', (req, res) => {
 		res.status(400).send(e);
 	});
 });
-
+//
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
+});
+
+// POST /users/login {email, password}
+
+app.post('/user/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+
+    return user.generateAuthToken().then((token) => {
+				res.header('x-auth', token).send(user);
+		});
+    //res.send({user});
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
 });
 
 app.listen(port, () => {
